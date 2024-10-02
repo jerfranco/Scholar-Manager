6d3ac162189f1c4f648e46aa6660bd75cfe0f918
Jeremiah Franco <jeremiahfranco3@gmail.com>
2024-10-02 15:51:21 -0600

    Trying to fix errors 5

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -5,11 +5,11 @@ const collection = require('./config');
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen (PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen (PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 
 app.use(express.json());
